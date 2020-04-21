012e4883546f0abc25138f6cefc221a10f338372
Tim Laurence <tim.laurence@bynorth.com>
2020-04-21 12:15:25 -0400

    Fixing problem in common plugin where plug functions were not registered

--- USB_Quartermaster_common/__init__.py
+++ USB_Quartermaster_common/__init__.py
@@ -1,4 +1,20 @@
-from .Communicator import AbstractCommunicator, CommunicatorError
-from .Driver import AbstractRemoteHostDriver, AbstractShareableDeviceDriver, AbstractLocalDriver
-from .Exceptions import USB_Quartermaster_Exception
-from .util import CommandResponse
+from .Communicator import \
+    AbstractCommunicator, \
+    CommunicatorError
+from .Driver import \
+    AbstractRemoteHostDriver, \
+    AbstractShareableDeviceDriver, \
+    AbstractLocalDriver
+from .Exceptions import \
+    USB_Quartermaster_Exception
+from .plugins import \
+    is_communicator, \
+    is_local_driver, \
+    is_remote_host_driver, \
+    is_shareable_device_driver, \
+    communicator_classes, \
+    local_driver_classes, \
+    remote_host_classes, \
+    shareable_device_classes
+from .util import \
+    CommandResponse
